91448ba0b1462ef799b07850bfddea098cbab9b2
nairadi <nairadi@users.noreply.github.com>
2022-03-03 00:58:00 -0500

    Code cleanup and testing optimizations

--- ctci/adi/2_06_Palindrome.py
+++ ctci/adi/2_06_Palindrome.py
@@ -26,40 +26,29 @@ def is_palindrome(root):
 
     left_index = 0
     right_index = len(ll_value_list) - 1
-    while(left_index < right_index):
-        if ll_value_list[left_index] != ll_value_list[right_index]:
-            return False
-        left_index += 1
-        right_index -= 1
+    # while(left_index < right_index):
+    #     if ll_value_list[left_index] != ll_value_list[right_index]:
+    #         return False
+    #     left_index += 1
+    #     right_index -= 1
 
-    return True
-
-node1 = Node(1, None)
-node2 = Node(2, None)
-node3 = Node(3, None)
-node4 = Node(4, None)
-node5 = Node(5, None)
-
-def create_ll(n1=None, n2=None, n3=None, n4=None, n5=None):
-    global node1, node2, node3, node4, node5
-    node1 = Node(n1, None)
-    node2 = Node(n2, None)
-    node3 = Node(n3, None)
-    node4 = Node(n4, None)
-    node5 = Node(n5, None)
+    # return True
+    return ll_value_list == ll_value_list[::-1]
     
-    if node1.val is None or node2.val is None:
-        return
-    node1.next = node2
-    if node3.val is None:
-        return
-    node2.next = node3
-    if node4.val is None:
-        return
-    node3.next = node4
-    if node5.val is None:
-        return
-    node4.next = node5
+def create_ll(int_list):
+    list_size = len(int_list)
+
+    if list_size < 1:
+        return None
+
+    head = Node(int_list[0], None)
+    prev = head
+
+    for element in int_list[1:]:
+        new_node = Node(element, None)
+        prev.next = new_node
+
+    return head
 
 
 # Time: O(n)
@@ -67,15 +56,15 @@ def create_ll(n1=None, n2=None, n3=None, n4=None, n5=None):
 
 class TestDeleteMiddle(unittest.TestCase):
     def test_solve(self):
-        create_ll(1,2,3,3,2)
-        self.assertEqual(is_palindrome(node1), False)
-        self.assertEqual(is_palindrome(node2), True)
-        create_ll()
-        self.assertEqual(is_palindrome(node1), True)
-        create_ll(1)
-        self.assertEqual(is_palindrome(node1), True)
-        create_ll(1,2,1)
-        self.assertEqual(is_palindrome(node1), True)
+        head = create_ll([1,2,3,3,2])
+        self.assertEqual(is_palindrome(head), False)
+        self.assertEqual(is_palindrome(head.next), True)
+        head = create_ll([])
+        self.assertEqual(is_palindrome(head), True)
+        head = create_ll([1])
+        self.assertEqual(is_palindrome(head), True)
+        head = create_ll([1,2,1])
+        self.assertEqual(is_palindrome(head), True)
 
 if __name__ == '__main__':
     unittest.main()
